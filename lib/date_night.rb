dafb08d9cef57c768faba90e55a32a3214f2ca50
Anna Lewis <anna.noel.lewis@gmail.com>
2017-08-20 16:16:21 -0600

    add methods to BinarySearchTree and Node classes

--- lib/date_night.rb
+++ lib/date_night.rb
@@ -1,15 +1,41 @@
+#   def depth_of(score)
+#     #return depth or nil if doesn't exist
+#   end
+#
+#   def include?(score)
+#     #return boolean, true if score is there
+#   end
+#
+#   def max
+#     #return highest movie and score: {movie_title, score}
+#   end
+#
+#   def min
+#     #return lowest movie and score: {movie_title, score}
+#   end
+#
+#   def sort
+#     #[{movie_title, score}, {movie_title, score}, {movie_title, score}, etc]
+#   end
+#
+#   def load(movies_txt)
+#     #number of movies added (ignore/don't add score repeats)
+#   end
+#
+# end
+
 class BinarySearchTree
 
-  def initialize
-    @score = 0
-    @movie_title = ''
+  def load(file)
+    root = Node.new(first_line_score_and_movie)
+    #iterate through list adding new nodes to root
   end
 
-  def insert(score, movie_title)
-    if @score > score
-      insert_right(score)
-    elsif @score < score
-      insert_left(score)
+  def insert(score, movie)
+    if @score < score
+      insert_right(score, movie_title)
+    elsif @score > score
+      insert_left(score, movie_title)
     else
       "that score already exists"
       #clarify with instructors what this should be
@@ -18,115 +44,33 @@ class BinarySearchTree
   end
 
   def insert_left(score, movie_title)
-    if left
-      left.insert(score, movie_title)
+    if left.nil?
+      @left = Node.new(score, movie_title)
     else
-      self.left = Node.new(score, movie_title)
+      @left.insert(score, movie_title)
     end
   end
 
   def insert_right(score, movie_title)
-    if right
-      right.insert(score, movie_title)
+    if right.nil?
+      @right = Node.new(score, movie_title)
     else
-      self.right = Node.new(score, movie_title)
+      @right.insert(score, movie_title)
     end
-  end
-
-  def depth_of(score)
-    #return depth or nil if doesn't exist
-  end
-
-  def include?(score)
-    #return boolean, true if score is there
-  end
-
-  def max
-    #return highest movie and score: {movie_title, score}
-  end
-
-  def min
-    #return lowest movie and score: {movie_title, score}
-  end
-
-  def sort
-    #[{movie_title, score}, {movie_title, score}, {movie_title, score}, etc]
-  end
-
-  def load(movies_txt)
-    #number of movies added (ignore/don't add score repeats)
   end
 
 end
 
 class Node
 
-  attr_reader :score, :movie_title, :depth
+  attr_reader :score, :movie_title
   attr_accessor :left, :right
 
-  def initialize(score, movie_title)
+  def initialize(score, movie)
     @score = score
-    @movie_title = movie_title
-    @left = Node.new(score, movie_title)
-    @right = Node.new(score, movie_title)
-    @depth = 0
+    @movie = movie
+    @left = nil
+    @right = nil
   end
 
 end
-
-
-
-# class Node
-#
-#   def initialize(score, movie_title)
-#     @score = score
-#     @movie_title = movie_title
-#     @left = EmptyNode.new
-#     @right = EmptyNode.new
-#     @depth = 0
-#   end
-#
-#   def include?
-#     case score
-#     when score > base_score
-#       then left.include?(score)
-#     when score < base_score
-#       then right.include?(score)
-#     when score == base_score
-#       then true # the current node is equal to the value
-#     end
-#   end
-#
-#   def insert_left(score)
-#     if left
-#       left.insert(score)
-#     else
-#       self.left = Node.new(score)
-#     end
-#   end
-#
-#   def insert_right(score)
-#     if right
-#       right.insert(score)
-#     else
-#       self.right = Node.new(score)
-#     end
-#   end
-#
-# end
-#
-# class EmptyNode
-#
-#   def to_hash
-#     {}
-#   end
-#
-#   def include?(*)
-#     false
-#   end
-#
-#   def insert(*)
-#     false
-#   end
-#
-# end
