1b7a7e65a801e3f812771e657a11d441846cc8ee
Anna Lewis <anna.noel.lewis@gmail.com>
2017-08-21 14:41:15 -0600

    seperate classes into seperate files and remove binding.pry

---
+++ lib/binary_tree.rb
@@ -0,0 +1,66 @@
+#   def depth_of(score)
+#     #return depth or nil if doesn't exist
+#   end
+#
+#   def max
+#     #return highest movie and score: {movie, score}
+#   end
+#
+#   def min
+#     #return lowest movie and score: {movie, score}
+#   end
+#
+#   def sort
+#     #[{movie, score}, {movie, score}, {movie, score}, etc]
+#   end
+#
+#   def load(movies_txt)
+#     #number of movies added (ignore/don't add score repeats)
+#   end
+#
+# end
+
+require 'pry'
+require_relative './node.rb'
+
+class BinarySearchTree
+
+  #look up smart vs. dumb nodes
+
+  # def load(file)
+  #   @root = Node.new(first_line_score_and_movie)
+  #   #iterate through list adding new nodes to root
+  #   #return number of movies added (ignore/don't add score repeats)
+  # end
+
+  def insert(score, movie)
+    if @root.nil?
+      @root = Node.new(score, movie, 0)
+      return @root.depth_counter
+    else
+      @root.insert(score, movie)
+    end
+  end
+
+  def include?(score)
+    if @root.score == score
+      return true
+    elsif @root.nil? || (@root.right.nil? && @root.left.nil?)
+      return false
+    elsif @root.right.nil? || @root.score > score
+      @root.left.include?(score)
+    elsif @root.left.nil? || @root.score < score
+      @root.right.include?(score)
+    end
+  end
+
+  def depth_of(score)
+    if @root.nil? || (@root.right.nil? && @root.left.nil?)
+      return nil
+    elsif @root.score == score
+      return 0
+    else
+      @root.depth_of(score)
+    end
+  end
+end
---
+++ lib/node.rb
@@ -0,0 +1,64 @@
+#make new file
+#make new test file foe node class
+class Node
+  attr_reader :score, :movie
+  attr_accessor :left, :right, :depth_counter
+
+  def initialize(score, movie, depth_counter)
+    @score = score
+    @movie = movie
+    @left = nil
+    @right = nil
+    @depth_counter = depth_counter
+  end
+
+  def insert(score, movie)
+    if @score < score
+      insert_right(score, movie)
+    elsif @score > score
+      insert_left(score, movie)
+    end
+  end
+
+  def insert_left(score, movie)
+    if left.nil?
+      @left = Node.new(score, movie, depth_counter + 1)
+      return @left.depth_counter
+    else
+      @left.insert(score, movie)
+    end
+  end
+
+  def insert_right(score, movie)
+    if @right.nil?
+      @right = Node.new(score, movie, depth_counter + 1)
+      return @right.depth_counter
+    else
+      @right.insert(score, movie)
+    end
+  end
+
+  def include?(score)
+    if @score == score
+      return true
+    elsif @right.nil? && @left.nil?
+      return false
+    elsif @right.right.nil? || @right.score > score
+      @left.include?(score)
+    elsif @left.left.nil? || @left.score < score
+      @right.include?(score)
+    end
+  end
+
+  def depth_of(score)
+    if @score == score
+      return @depth_counter
+    elsif @right.nil? && @left.nil?
+      return nil
+    elsif @right.nil? || @score >= score
+      @left.depth_of(score)
+    elsif @left.nil? || @score <= score
+      @right.depth_of(score)
+    end
+  end
+end
